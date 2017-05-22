ed8d115ab85be8c2d3529b790ab1b4bdac94c5dc
Kanishk Gandhi <kanishkg@iitk.ac.in>
2017-05-22 18:24:51 -0400

    debug

--- scripts/index_file.py
+++ scripts/index_file.py
@@ -2,26 +2,41 @@ import os
 import numpy
 import h5py
 import pickle
+from utils import ProgressBar
 
 lag = 3
 num_frames = 4
 
-file_dir = '../../../../scratch/kvg245/vidsal_gan/vidsal_gan/data/ftp.ivc.polytech.univ-nantes.fr/IRCCyN_IVC_Eyetracker_SD_2009_12/H264_Streams/'
+file_dir = '../../../../scratch/kvg245/vidsal_gan/vidsal_gan/data/savam/video_data/'
 
-files_list= [x for x in os.listdir(file) ]
+files_list= [x for x in os.listdir(file_dir) if x.endswith('avi')]
+print sorted(files_list)
+print len(files_list)
 data_list = []
-for file in files_list:
-    with h5py.File(file_dir+file,'r') as hf:
-        data_list.append(hf['frames'][:])
+print "reading file"
+progress = ProgressBar(len(files_list),fmt=ProgressBar.FULL)
 index_list = []
-h = h5py.File('inputs.h5', 'w')
-for v in range(len(data_list)):
-    index_list.append([])
-    h.create_dataset(str(v),data = data_list[v])
-    for f in range(data_list[v].shape[0]):
-        if f<num_frames*lag-1:
-            pass
-        for n in range (num_frames):
-            index_list[v].append([f-n*lag])
-with open('indices','w') as ifile:
+v = 0
+with h5py.File(file_dir+'video_data.h5','r') as hf:
+    for file in sorted(files_list):
+    	progress.current+=1
+	progress()
+	data = hf[file][:]
+	 
+
+	
+	index_list.append([])
+        for f in range(data.shape[0]):
+	   
+            
+            if f<num_frames*lag-lag:
+                pass
+	    else:
+                index_list[v].append([])
+                for n in range (num_frames):
+                    index_list[v][f-num_frames*lag+lag].append(f-n*lag)
+        v+=1
+progress.done()
+print "saving pickle"
+with open(file_dir+'indices','w') as ifile:
     pickle.dump(index_list,ifile)
--- scripts/input_process.py
+++ scripts/input_process.py
@@ -3,10 +3,11 @@ import os
 import h5py
 import cv2
 import multiprocessing
-
-video_dir ="../../../../scratch/kvg245/vidsal_gan/vidsal_gan/data/savam/video_data/"
+from utils import ProgressBar
+video_dir ="../../../../scratch/kvg245/vidsal_gan/vidsal_gan/data/savam/gaussian_vizualizations/"
 
 def get_video_frames(filename):
+    global progress
     print ("Opening video!",video_dir+filename)
     capture = cv2.VideoCapture(video_dir+filename)
     print ("Video opened\nChoosing frames")
@@ -20,24 +21,30 @@ def get_video_frames(filename):
         if frame is None :
             break
         frame = cv2.resize(frame, (224, 224)).astype(np.float32)
-
+	print i
         frames.append(np.asarray(frame))
         if ret == False:
             break
         i+=1
 
     frames = np.array(frames)#convert to (num_frames, width, height, depth)
-
+    
     print ("Frames chosen")
     print ("Length of video %d" % frames.shape[0])
-    with h5py.File(video_dir++'video_data.h5', 'a') as hf:
+    with h5py.File(video_dir+'video_data.h5', 'a') as hf:
         hf.create_dataset(filename,  data=frames)
-
+    progress.current+=1
+    progress()
     return video_dir+filename
 
 
+c = [6, 11, 13, 22, 23, 26, 30, 35, 41]
+h = h5py.File(video_dir+'video_data.h5')
+video_list = [x for x in os.listdir(video_dir) if x.endswith('.avi') and x not in h.keys()]
+print [x[:3]for x in video_list]
+h.close()
+progress = ProgressBar(len(video_list))
 
-video_list = [x for x in os.listdir(video_dir) if x.endswith('.avi') ]
-print video_list
-p = multiprocessing.Pool(5)
-print p.map(get_video_frames,video_list)                                            
+p = multiprocessing.Pool(1)
+p.map(get_video_frames,video_list)
+progress.done()                                            
